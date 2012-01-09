b906dd3e79c379d31ce230e6a3782d702146b23e
charlesmarshall <cm56marshall@googlemail.com>
2012-01-09 19:02:43 +0000

    hook to handle the new joins

--- lib/controller/CMSAdminCustomformbuilderController.php
+++ lib/controller/CMSAdminCustomformbuilderController.php
@@ -6,6 +6,26 @@ class CMSAdminCustomformbuilderController extends AdminComponent {
   public $display_name = "Form Builder";
   public $dashboard = false;
 
+  protected function events(){
+    parent::events();
+    WaxEvent::add("cms.joins.handle", function(){
+      $obj = WaxEvent::data();
+	    $saved = $obj->model;
+	    $saved->fields->unset($model->fields); //clear the joins every save	    
+	    //handle new fields
+	    foreach(Request::param('new_field') as $field){
+	      $model = new WildfireCustomField;
+        if($s = $model->update_attributes($field)) $saved->fields = $s;
+	    }
+	    //handle existing joins
+	    foreach(Request::param('fields') as $field){
+	      $model = new WildfireCustomField($field['primval']);
+	      unset($field['primval']);
+	      if($s = $model->update_attributes($field)) $saved->fields = $s;
+	    }
+    });
+  }
+
 }
 
 ?>
