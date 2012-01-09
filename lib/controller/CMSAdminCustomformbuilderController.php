a0414d92d8f3b145bd9008c7bfdba6df767d149b
charlesmarshall <cm56marshall@googlemail.com>
2012-01-09 17:07:13 +0000

    start on the cms part of the form builder

---
+++ lib/controller/CMSAdminCustomformbuilderController.php
@@ -0,0 +1,11 @@
+<?
+class CMSAdminCustomformbuilderController extends AdminComponent {
+
+  public $module_name = "customformbuilder";												
+  public $model_class = 'WildfireCustomForm';
+	public $display_name = "Site Categories";
+  public $dashboard = false;
+
+}
+
+?>
