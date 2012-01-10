08eaf71988777e74482b2621aea8ae86af524e8f
charlesmarshall <cm56marshall@googlemail.com>
2012-01-10 10:47:43 +0000

    jquery to sort the form fields, simply update the hidden field to have the order value in

---
+++ resources/public/javascripts/wildfire-wildfire.formbuilder/sorting.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+  
+  jQuery("ul.existing_field").sortable({
+    update:function(e,ui){
+      var obj = jQuery(this), 
+          parent = obj.parent(), 
+          ordering = {}, 
+          items = parent.find("li"),
+          lic=0
+          ;
+      items.each(function(){
+        jQuery(this).find(".hidden_field").val(lic);
+        lic++;
+      });
+      
+    }
+  }).disableSelection();
+  
+});
