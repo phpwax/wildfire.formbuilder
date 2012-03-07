486063749e7b95e83f169e83a12a92499b277a99
charles marshall <charles@oneblackbear.com>
2012-03-07 13:02:00 +0000

    allow for sending to multiple email addresses

--- lib/model/Notification.php
+++ lib/model/Notification.php
@@ -3,7 +3,9 @@ class Notification extends WaxEmail{
 
 
   public function submission($data, $to, $subject="Notification of form submission", $dev_emails=array()){
-    $this->add_to_address($to);
+    $all = explode(",", str_replace(";", ",", $to));
+    $this->add_to_address(array_unshift($all));
+    foreach($all as $em) $this->add_cc_address($em);
     $this->data = $data;
     $this->subject = $subject;
     foreach($dev_emails as $em) $this->add_bcc_address($em);
