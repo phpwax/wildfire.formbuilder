80240ecadc08f306050e920f104555fe85998cdf
charles marshall <charles@oneblackbear.com>
2012-03-27 10:55:01 +0200

    shift, not unshift

--- lib/model/Notification.php
+++ lib/model/Notification.php
@@ -5,7 +5,7 @@ class Notification extends WaxEmail{
   public function submission($data, $to, $from, $subject="Notification of form submission", $dev_emails=array()){
     $this->from = $from;
     $all = explode(",", str_replace(";", ",", $to));
-    $this->add_to_address(array_unshift($all));
+    $this->add_to_address(array_shift($all));
     foreach($all as $em) $this->add_cc_address($em);
     $this->data = $data;
     $this->subject = $subject;
