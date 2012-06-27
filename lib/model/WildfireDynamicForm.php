b33893abdcbceec8387a61b25c56252714ce1805
charles marshall <charles@oneblackbear.com>
2012-06-27 16:03:45 +0200

    include a column to track submission date

--- lib/model/WildfireDynamicForm.php
+++ lib/model/WildfireDynamicForm.php
@@ -7,6 +7,7 @@ class WildfireDynamicForm extends WaxModel{
   public function setup(){
      parent::setup();
      $this->define("form", "IntegerField", array('widget'=>'HiddenInput'));
+     $this->define("date_submitted", "DateTimeField");
      WaxEvent::run(get_class($this).".setup", $this);
    }
 
