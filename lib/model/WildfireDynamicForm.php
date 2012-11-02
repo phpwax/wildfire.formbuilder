6e8060b26acf1279ea71bf5de57c9d968f49a600
charlesmarshall <cm56marshall@googlemail.com>
2012-11-02 11:17:53 +0000

    add in a saved on date

--- lib/model/WildfireDynamicForm.php
+++ lib/model/WildfireDynamicForm.php
@@ -1,15 +1,16 @@
 <?
 class WildfireDynamicForm extends WaxModel{
-  
+
   public static $form;
   public static $custom_form_fields=array();
-  
+
   public function setup(){
      parent::setup();
      $this->define("form", "IntegerField", array('widget'=>'HiddenInput'));
+     $this->define("submitted_on", "DateTimeField", array('widget'=>'HiddenInput', 'default'=>"now"));
      WaxEvent::run(get_class($this).".setup", $this);
    }
 
-  
+
 }
 ?>
