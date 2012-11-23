7050b2ffea4a20deb676bd3a4335adf5ae09c17c
charlesmarshall <cm56marshall@googlemail.com>
2012-11-23 18:28:29 +0000

    fixes to the bot check and add db sync

--- lib/controller/WildfireFormbuilderController.php
+++ lib/controller/WildfireFormbuilderController.php
@@ -1,6 +1,6 @@
 <?
 class WildfireFormbuilderController extends WaxController{
-  
+
   public $custom_form = false;
   public $custom_form_model = false;
   public $form_class = "WildfireCustomForm";
@@ -31,16 +31,17 @@ class WildfireFormbuilderController extends WaxController{
         if($field->subtext) $field->title .= "<br><span class='subtext'>".$field->subtext."</span>";
         //set the group
         $group = $field->field_group;
-        
-        
+
+
         if($field->choices){
           $c = explode("\n", $field->choices);
           foreach($c as $v) $choices[trim($v)] = trim($v);
         }
         $previous_group = $group;
-        $options = array('widget'=>$field->field_type, 'original_title'=>$field->original_title, 'label'=>$field->title, 'required'=>$field->required, 'choices'=>$choices, 'fg'=>$field->field_group, 'extra_class'=>$field->extra_class);          
+        $options = array('widget'=>$field->field_type, 'original_title'=>$field->original_title, 'label'=>$field->title, 'required'=>$field->required, 'choices'=>$choices, 'fg'=>$field->field_group, 'extra_class'=>$field->extra_class);
         $obj->define($field->column_name, "CharField", $options);
       }
+      $obj->syncdb();
     });
 
   }
@@ -52,9 +53,9 @@ class WildfireFormbuilderController extends WaxController{
 
     if($this->form_primval && ($custom_form = $fm_class::$form_model = new $f_class($this->form_primval))){
       $fm_class::$form = $this->custom_form_model = $custom_form; //copy to this var so we can access the content / t&cs etc
- 
+
       $this->events($fm_class);
-      
+
       $db = new $fm_class;
       $this->custom_form = new WaxForm($db);
       //bot check functionality
@@ -63,13 +64,14 @@ class WildfireFormbuilderController extends WaxController{
         if(($to = $custom_form->email_notification) && ($this->email_notification_class)){
           $sender = new $this->email_notification_class;
           $func = "send_".$this->email_notification_func;
-          if($session_data = (array)unserialize( Session::get('campaign_data'))) $saved->row = array_merge($saved->row, array_filter($session_data));
+
+          if($session_data = (array) unserialize( Session::get('campaign_data'))  ) $saved->row = array_merge($saved->row, array_filter($session_data));
           if($this->custom_form_model->email_subject) $sender->$func($saved, $to, $this->from_address, $this->custom_form_model->email_subject);
           elseif($this->email_subject && $this->dev_emails) $sender->$func($saved, $to, $this->from_address, $this->email_subject, $this->dev_emails);
           elseif($this->email_subject) $sender->$func($saved, $to, $this->from_address, $this->email_subject);
           else $sender->$func($saved, $to, $this->from_address);
         }
-        $this->redirect_to($custom_form->redirect_to_after_save."?s=".$saved->primval);
+        $this->redirect_to($custom_form->redirect_to_after_save."?s=".$saved->primval, false);
       }
     }
   }
@@ -87,8 +89,9 @@ class WildfireFormbuilderController extends WaxController{
     $custom_form = false;
 
     $dynamic_form = new $fm_class;
+    ini_set("max_execution_time", 0);
     $page = 1;
-    $per_page = 1000;
+    $per_page = 100;
     while(($dynamic_form = $dynamic_form->page($page,$per_page)) && $page <= $dynamic_form->total_pages){
       foreach($dynamic_form as $d_form){
         print_r("Working on: ".$d_form->id."\n");
@@ -108,9 +111,9 @@ class WildfireFormbuilderController extends WaxController{
 
   protected function bot_check(){
     $posted = Request::param('check-in');
-    if(($v = array_shift($posted)) && $v) return true;
+    if($posted && ($v = array_shift($posted)) && $v) return true;
     else return false;
   }
-  
+
 }
 ?>
