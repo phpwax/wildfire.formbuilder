890dcb09c2fb093d79b35b0988a9b6ad8ae121cd
Charles Marshall <charles@oneblackbear.com>
2013-06-25 12:22:21 +0100

    covert over for composer style setup

--- setup.php
+++ setup.php
@@ -1,4 +1,11 @@
 <?
+
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.formbuilder","dir"=>__DIR__);
+
+
 CMSApplication::register_module("customformbuilder", array('plugin_name'=>'wildfire.formbuilder', 'assets_for_cms'=>true, "display_name"=>"Form Builder", "link"=>"/admin/customformbuilder/", 'split'=>true));
 
 if(!defined("CONTENT_MODEL")){
@@ -10,4 +17,5 @@ WaxEvent::add(CONTENT_MODEL.".setup", function(){
   $model = WaxEvent::data();
   if(!$model->columns['forms']) $model->define("forms", "ManyToManyField", array('target_model'=>'WildfireCustomForm', 'group'=>'relationships'));
 });
+
 ?>
