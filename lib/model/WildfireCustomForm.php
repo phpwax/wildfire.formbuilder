2520ccea57a6e374bdb9576a22ae48835d038d02
mikenosko <michal@oneblackbear.com>
2013-10-11 15:57:29 +0100

    trimming the table name to ~ 40 chars

--- lib/model/WildfireCustomForm.php
+++ lib/model/WildfireCustomForm.php
@@ -29,7 +29,7 @@ class WildfireCustomForm extends WaxModel{
   }
   
   public function get_prefix($test=false){
-    if(!$test) $test = Inflections::underscore(Inflections::to_url(str_replace("/", "_", $this->title)));
+    if(!$test) $test = substr(Inflections::underscore(Inflections::to_url(str_replace("/", "_", $this->title))),0,40);
     $model = new WildfireCustomForm;
     if($model->filter("prefix", $test)->first()) return $this->get_prefix($test.rand(1000,9999));
     else return $test;
