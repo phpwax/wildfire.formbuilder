989bd63565078c3ab687cddd0ce3afb6403c6f18
Charles Marshall <charles@oneblackbear.com>
2013-10-16 10:36:34 +0100

    limit length on table name

--- lib/model/WildfireCustomForm.php
+++ lib/model/WildfireCustomForm.php
@@ -30,7 +30,7 @@ class WildfireCustomForm extends WaxModel{
   }
 
   public function get_prefix($test=false){
-    if(!$test) $test = Inflections::underscore(Inflections::to_url(str_replace("/", "_", $this->title)));
+    if(!$test) $test = substr( Inflections::underscore(Inflections::to_url(str_replace("/", "_", $this->title))), 0, 12) ;
     $model = new WildfireCustomForm;
     if($model->filter("prefix", $test)->first()) return $this->get_prefix($test.rand(1000,9999));
     else return $test;
